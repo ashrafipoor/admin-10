5e2c15da5beed94a0195f856ece7db45de3fa5fa
houssein ashrafipoor <ashrafipoor@gmail.com>
2022-04-25 20:25:32 +0200

    connect userList to API

--- src/Components/Users/UserList.js
+++ src/Components/Users/UserList.js
@@ -1,23 +1,28 @@
+import { useEffect, useState } from 'react';
 import './UserList.css';
 import UserListItem from './UserListItem';
 function UserList() {
-    const userInfo={
-        fullName:'Hart Hagerty',
-        country:'United States',
-        company:'Zemlak, Daniel and Leannon',
-        role:'Desktop Support Technician',
-        image:'https://api.lorem.space/image/face?w=150&h=150'
 
-    }
+    const [myClients, setMyClients] = useState([]);
+    useEffect(() => {
+        fetch('https://myapi.run/api/clients?populate=*', {
+            method: 'get', headers: {
+                'Authorization': 'Bearer d8de5bbc6afc2e274d6d6dc411e9a53a70236ced8106b7ff5cc4806525c123d995252b5c7e6d99e68db6cd224dbbb61a559382d9a9b22a0cbe6c66acd23e0af8849f24f01543e53d3c6611adbcd8f89a272137db2a74d373ecae6d51e5c0b644a6e4e143394194e20d1ee2de4663bcee188cf71b881af55d2cb1054a30f02272',
+            }
+        })
+            .then(res => res.json())
+            .then(result => setMyClients(result.data))
+
+    }, [])
     return (
         <div className=" w-full h-screen ">
-            <table class="table w-full">
+            <table className="table w-full">
 
                 <thead>
                     <tr>
                         <th>
                             <label>
-                                <input type="checkbox" class="checkbox" />
+                                <input type="checkbox" className="checkbox" />
                             </label>
                         </th>
                         <th>Name</th>
@@ -26,15 +31,20 @@ function UserList() {
                     </tr>
                 </thead>
                 <tbody>
-                    <UserListItem {...userInfo}/>
-                    <UserListItem {...userInfo}/>
-                    <UserListItem {...userInfo}/>
-                    <UserListItem {...userInfo}/>
-                    <UserListItem {...userInfo}/>
-                    <UserListItem {...userInfo}/>
-                    <UserListItem {...userInfo}/>
-                    <UserListItem {...userInfo}/>
-                  
+
+
+                {myClients.map(clientInfo=><UserListItem
+                key={clientInfo.id}
+                image={'https://myapi.run/'+clientInfo.attributes.image.data.attributes.url}
+                fullName={clientInfo.attributes.fullName}
+                country={clientInfo.attributes.country}
+                company={clientInfo.attributes.company}
+                role={clientInfo.attributes.role}
+
+                  />)}
+                    
+                   
+
                 </tbody>
 
 
--- src/Components/Users/UserListItem.js
+++ src/Components/Users/UserListItem.js
@@ -7,29 +7,29 @@ function UserListItem(props){
         <tr>
         <th>
             <label>
-                <input type="checkbox" class="checkbox" />
+                <input type="checkbox" className="checkbox" />
             </label>
         </th>
         <td>
-            <div class="flex items-center space-x-3">
-                <div class="avatar">
-                    <div class="mask mask-squircle w-12 h-12">
+            <div className="flex items-center space-x-3">
+                <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
                         <img src={props.image} alt="Avatar Tailwind CSS Component" />
                     </div>
                 </div>
                 <div>
-                    <div class="font-bold">{props.fullName}</div>
-                    <div class="text-sm opacity-50">{props.country}</div>
+                    <div className="font-bold">{props.fullName}</div>
+                    <div className="text-sm opacity-50">{props.country}</div>
                 </div>
             </div>
         </td>
         <td>
             {props.company}
             <br />
-            <span class="badge badge-ghost badge-sm">{props.role}</span>
+            <span className="badge badge-ghost badge-sm">{props.role}</span>
         </td>
         <th>
-            <button class="btn btn-ghost btn-xs">details</button>
+            <button className="btn btn-ghost btn-xs">details</button>
         </th>
     </tr>
 
